62c718469247892a9bbcaa13a69dc1be12152325
SilviuDinu <1324luky2GitHub>
2019-10-18 22:42:51 +0300

    flip card login

--- assets/src/components/render_login.vue
+++ assets/src/components/render_login.vue
@@ -7,26 +7,10 @@
             }
         },
         methods: {
-            register: function(){
-                const loginForm = document.querySelector('form#login');
-                const regForm = document.querySelector('form#register');
-                if(loginForm.style.display === '' || loginForm.style.display === 'block'){
-                    jQuery( loginForm ).fadeOut(600);
-                    jQuery( regForm ).fadeIn(600);
-                    // jQuery( loginForm ).slideUp(500).delay(200);
-                    // jQuery( regForm ).slideDown();
-                }
-
-            },
-            backToLogin: function(){
-                const loginForm = document.querySelector('form#login');
-                const regForm = document.querySelector('form#register');
-                if(loginForm.style.display === 'none'){
-                    jQuery( regForm ).fadeOut(600);
-                    jQuery( loginForm ).fadeIn(600);
-                    // jQuery( regForm ).slideUp().delay(200);
-                    // jQuery( loginForm ).slideDown(500);
-                }
+            register: function () {
+                const card = document.querySelector('div.flip-card-child');
+                const front = document.querySelector('div.flip-card-front');
+                card.classList.toggle('flipped');
             },
             rise: function () {
                 event.target.classList.add('focus');
@@ -44,46 +28,57 @@
 </script>
 
 <template>
-    <div>
-        <form id="login" action="login" class="login-form" method="post">
-            <h1>Login</h1>
-            <div class="txtb">
-                <input type="text" name="username_login" pattern="[A-Za-z0-9]+" @focus="rise()" @blur="fall()">
-                <span data-placeholder="Username"></span>
-            </div>
+    <div class="flip-card-parent">
+        <div class="flip-card-child">
+            <div class="flip-card-front">
+                <form id="login" action="login" class="login-form" method="post" >
+                    <h1>Login</h1>
+                    <div class="txtb">
+                        <input type="text" name="username_login" pattern="[A-Za-z0-9]+" @focus="rise()" @blur="fall()">
+                        <span data-placeholder="Username"></span>
+                    </div>
 
-            <div class="txtb">
-                <input type="password" name="password_login" pattern="[A-Za-z0-9]+" @focus="rise()" @blur="fall()">
-                <span data-placeholder="Password"></span>
-            </div>
-            <input type="submit" class="logbtn" value="Login">
+                    <div class="txtb">
+                        <input type="password" name="password_login" pattern="[A-Za-z0-9]+" @focus="rise()"
+                               @blur="fall()">
+                        <span data-placeholder="Password"></span>
+                    </div>
+                    <input type="submit" class="logbtn" value="Login">
 
-            <div style="font-size: 16px" class="bottom-text">
-                Don't have account?
-                <button class="regbtn" @click="register()"><a style="font-size: 16px" href="#register">Sign up</a></button>
+                    <div style="font-size: 16px" class="bottom-text">
+                        Don't have account?
+                        <button class="regbtn" @click="register()"><a style="font-size: 16px" href="#register">Sign
+                            up</a></button>
+                    </div>
+                </form>
             </div>
-        </form>
-        <form id="register" action="register" class="register-form" method="post" style="display: none">
-            <h1>Register</h1>
-            <div class="txtb">
-                <input type="text" required name="username_register" pattern="[A-Za-z0-9]+" @focus="rise()"
-                       @blur="fall()">
-                <span data-placeholder="Username"></span>
-            </div>
+            <div class="flip-card-back">
+                <form id="register" action="register" class="register-form" method="post">
+                    <h1>Register</h1>
+                    <div class="txtb">
+                        <input type="text" required name="username_register" pattern="[A-Za-z0-9]+" @focus="rise()"
+                               @blur="fall()">
+                        <span data-placeholder="Username"></span>
+                    </div>
 
-            <div class="txtb">
-                <input type="password" required name="password_register" pattern="[A-Za-z0-9]+" @focus="rise()"
-                       @blur="fall()">
-                <span data-placeholder="Password"></span>
-            </div>
+                    <div class="txtb">
+                        <input type="password" required name="password_register" pattern="[A-Za-z0-9]+" @focus="rise()"
+                               @blur="fall()">
+                        <span data-placeholder="Password"></span>
+                    </div>
 
-            <div class="txtb">
-                <input type="email" required name="email_register" pattern="[A-Za-z0-9-_@.]+" @focus="rise()"
-                       @blur="fall()">
-                <span data-placeholder="Email"></span>
+                    <div class="txtb">
+                        <input type="email" required name="email_register" pattern="[A-Za-z0-9-_@.]+" @focus="rise()"
+                               @blur="fall()">
+                        <span data-placeholder="Email"></span>
+                    </div>
+                    <input type="submit" class="logbtn" value="Register">
+                    <div class="bottom-text">
+                        <button class="regbtn" @click="register()"><a style="font-size: 16px" href="#login">&larr;
+                            Back to Login</a></button>
+                    </div>
+                </form>
             </div>
-            <input type="submit" class="logbtn" value="Register">
-            <div class="bottom-text"><button class="regbtn" @click="backToLogin()"><a style="font-size: 16px" href="#login">&larr; Back to Login</a></button></div>
-        </form>
+        </div>
     </div>
 </template>
