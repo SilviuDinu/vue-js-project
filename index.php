46dd5f6501f24294e206a79af5e4961351d1a8c7
SilviuDinu <1324luky2GitHub>
2019-10-31 16:17:35 +0200

    not important

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ session_abort();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <script src="assets/js/build.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900|Ubuntu:400,500,700" rel="stylesheet">
 </head>
